0c35a875e593bb7e01084b62f7dc1c5254b3817a
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2022-09-06 02:01:58 +0200

    delete Layout from App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Layout from './components/Layout/Layout';
 import Routes from './components/Routes/Routes';
 
 function App() {
