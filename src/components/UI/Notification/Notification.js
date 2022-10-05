234b974df3fe959ce78b29171e1f10c1c89565d4
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2022-10-05 04:09:22 +0200

    Update Notification.js

--- src/components/UI/Notification/Notification.js
+++ src/components/UI/Notification/Notification.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import './Notification.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Notification.css";
+import { Link } from "react-router-dom";
 
 export const Notification = ({ show, close, text }) => {
   if (!show) {
-    console.log('show', show);
+    console.log("show", show);
     return null;
   } else {
-    console.log('show', show, text);
+    console.log("show", show, text);
   }
 
   const showNotificationText = () => {
     switch (text) {
-      case 'ADD_TO_CART':
+      case "ADD_TO_CART":
         return (
           <>
-            Item added to{' '}
+            Item added to{" "}
             {
               <Link to="/cart">
-                <span style={{ textDecoration: 'underline', color: '#fff' }}>
+                <span style={{ textDecoration: "underline", color: "#fff" }}>
                   Cart
                 </span>
               </Link>
@@ -28,13 +28,13 @@ export const Notification = ({ show, close, text }) => {
         );
       // break;
 
-      case 'REMOVE_FROM_CART':
+      case "REMOVE_FROM_CART":
         return (
           <>
-            Item removed from{' '}
+            Item removed from{" "}
             {
               <Link
-                style={{ textDecoration: 'underline', color: '#fff' }}
+                style={{ textDecoration: "underline", color: "#fff" }}
                 to="/cart"
               >
                 Cart
@@ -44,26 +44,19 @@ export const Notification = ({ show, close, text }) => {
           </>
         );
 
-      case 'ITEM_QTY_INCREASED':
+      case "ITEM_QTY_INCREASED":
         return <>Item Quantity increased.</>;
 
-      case 'ITEM_QTY_DECREASED':
+      case "ITEM_QTY_DECREASED":
         return <>Item Quantity decreased.</>;
 
-      case 'ADDRESS_UPDATED':
+      case "ADDRESS_UPDATED":
         return <>Customer Address updated.</>;
 
-      case 'ADDRESS_UPDATED':
-        return (
-          <>
-            Customer Address updated.
-          </>
-        )
-
-      case 'ORDER_CANCELED':
+      case "ORDER_CANCELED":
         return <>Order canceled..</>;
 
-      case 'ORDER_ACCEPTED':
+      case "ORDER_ACCEPTED":
         return <>Order accepted..</>;
       default:
         return <>{text}</>;
@@ -77,20 +70,20 @@ export const Notification = ({ show, close, text }) => {
       <div
         className="notification"
         style={{
-          opacity: show ? '1' : '0',
-          transform: show ? 'translateY(0vh)' : 'translateY(-100vh)',
+          opacity: show ? "1" : "0",
+          transform: show ? "translateY(0vh)" : "translateY(-100vh)",
         }}
       >
         <div className="notification-content">
           <i
             className="fa fa-check-circle fa-2x"
-            style={{ color: '#1BD4A5', marginBottom: '9.6px' }}
+            style={{ color: "#1BD4A5", marginBottom: "9.6px" }}
           ></i>
           <p>{showNotificationText()}</p>
           <div
             onClick={close}
             className="close-button"
-            style={{ marginBottom: '0.7rem' }}
+            style={{ marginBottom: "0.7rem" }}
           >
             X
           </div>
